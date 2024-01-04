b3bb321a594b7a8db651c4820e175420a6de1526
Pavel Mikhalkevich <paul.michelle.prod@gmail.com>
2024-01-04 12:01:44 +0500

    Update real tests crate

--- tests/real/enterprise.rs
+++ tests/real/enterprise.rs
@@ -319,3 +319,368 @@ fn ent_unique_job_until_start() {
 
     handle.join().expect("should join successfully");
 }
+
+#[test]
+fn test_tracker_can_send_and_retrieve_job_execution_progress() {
+    use std::{
+        io,
+        sync::{Arc, Mutex},
+        thread, time,
+    };
+
+    skip_if_not_enterprise!();
+
+    let url = learn_faktory_url();
+
+    let tracker = Arc::new(Mutex::new(
+        Tracker::connect(Some(&url)).expect("job progress tracker created successfully"),
+    ));
+    let tracker_captured = Arc::clone(&tracker);
+
+    let mut producer = Producer::connect(Some(&url)).unwrap();
+    let job_tackable = JobBuilder::new("order")
+        .args(vec![Value::from("ISBN-13:9781718501850")])
+        .queue("test_tracker_can_send_progress_update")
+        .build_trackable();
+
+    let job_ordinary = JobBuilder::new("order")
+        .args(vec![Value::from("ISBN-13:9781718501850")])
+        .queue("test_tracker_can_send_progress_update")
+        .build();
+
+    // let's remember this job's id:
+    let job_id = job_tackable.id().to_owned();
+    let job_id_captured = job_id.clone();
+
+    producer.enqueue(job_tackable).expect("enqueued");
+
+    let mut consumer = ConsumerBuilder::default();
+    consumer.register("order", move |job| -> io::Result<_> {
+        // trying to set progress on a community edition of Faktory will give:
+        // 'an internal server error occurred: tracking subsystem is only available in Faktory Enterprise'
+        let result = tracker_captured
+            .lock()
+            .expect("lock acquired")
+            .set_progress(
+                ProgressUpdateBuilder::new(&job_id_captured)
+                    .desc("I am still reading it...".to_owned())
+                    .percent(32)
+                    .build(),
+            );
+        assert!(result.is_ok());
+        // let's sleep for a while ...
+        thread::sleep(time::Duration::from_secs(2));
+
+        // ... and read the progress info
+        let result = tracker_captured
+            .lock()
+            .expect("lock acquired")
+            .get_progress(job_id_captured.clone())
+            .expect("Retrieved progress update over the wire");
+
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert_eq!(result.jid, job_id_captured.clone());
+        assert_eq!(result.state, "working");
+        assert!(result.updated_at.is_some());
+        assert_eq!(result.desc, Some("I am still reading it...".to_owned()));
+        assert_eq!(result.percent, Some(32));
+
+        // considering the job done
+        Ok(eprintln!("{:?}", job))
+    });
+
+    let mut consumer = consumer
+        .connect(Some(&url))
+        .expect("Successfully ran a handshake with 'Faktory'");
+    let had_one_job = consumer
+        .run_one(0, &["test_tracker_can_send_progress_update"])
+        .expect("really had one");
+
+    assert!(had_one_job);
+
+    let result = tracker
+        .lock()
+        .expect("lock acquired successfully")
+        .get_progress(job_id.clone())
+        .expect("Retrieved progress update over the wire once again")
+        .expect("Some progress");
+
+    assert_eq!(result.jid, job_id);
+    // 'Faktory' will be keeping last known update for at least 30 minutes:
+    assert_eq!(result.desc, Some("I am still reading it...".to_owned()));
+    assert_eq!(result.percent, Some(32));
+
+    // But it actually knows the job's real status, since the consumer (worker)
+    // informed it immediately after finishing with the job:
+    assert_eq!(result.state, "success");
+
+    // What about 'ordinary' job ?
+    let job_id = job_ordinary.id().to_owned().clone();
+
+    // Sending it ...
+    producer
+        .enqueue(job_ordinary)
+        .expect("Successfuly send to Faktory");
+
+    // ... and asking for its progress
+    let result = tracker
+        .lock()
+        .expect("lock acquired")
+        .get_progress(job_id.clone())
+        .expect("Retrieved progress update over the wire once again")
+        .expect("Some progress");
+
+    // From the docs:
+    // There are several reasons why a job's state might be unknown:
+    //    The JID is invalid or was never actually enqueued.
+    //    The job was not tagged with the track variable in the job's custom attributes: custom:{"track":1}.
+    //    The job's tracking structure has expired in Redis. It lives for 30 minutes and a big queue backlog can lead to expiration.
+    assert_eq!(result.jid, job_id);
+
+    // Returned from Faktory: '{"jid":"f7APFzrS2RZi9eaA","state":"unknown","updated_at":""}'
+    assert_eq!(result.state, "unknown");
+    assert!(result.updated_at.is_none());
+    assert!(result.percent.is_none());
+    assert!(result.desc.is_none());
+}
+
+#[test]
+fn test_batch_of_jobs_can_be_initiated() {
+    skip_if_not_enterprise!();
+    let url = learn_faktory_url();
+
+    let mut producer = Producer::connect(Some(&url)).unwrap();
+    let mut consumer = ConsumerBuilder::default();
+    consumer.register("thumbnail", move |_job| -> io::Result<_> { Ok(()) });
+    consumer.register("clean_up", move |_job| -> io::Result<_> { Ok(()) });
+    let mut consumer = consumer.connect(Some(&url)).unwrap();
+    let mut tracker =
+        Tracker::connect(Some(&url)).expect("job progress tracker created successfully");
+
+    let job_1 = Job::builder("thumbnail")
+        .args(vec!["path/to/original/image1"])
+        .queue("test_batch_of_jobs_can_be_initiated")
+        .build();
+    let job_2 = Job::builder("thumbnail")
+        .args(vec!["path/to/original/image2"])
+        .queue("test_batch_of_jobs_can_be_initiated")
+        .build();
+    let job_3 = Job::builder("thumbnail")
+        .args(vec!["path/to/original/image3"])
+        .queue("test_batch_of_jobs_can_be_initiated")
+        .build();
+
+    let cb_job = Job::builder("clean_up")
+        .queue("test_batch_of_jobs_can_be_initiated__CALLBACKs")
+        .build();
+
+    let batch =
+        Batch::builder("Image resizing workload".to_string()).with_complete_callback(cb_job);
+
+    let time_just_before_batch_init = Utc::now();
+
+    let mut batch = producer.start_batch(batch).unwrap();
+
+    // let's remember batch id:
+    let batch_id = batch.id().to_string();
+
+    batch.add(job_1).unwrap();
+    batch.add(job_2).unwrap();
+    batch.add(job_3).unwrap();
+    batch.commit().unwrap();
+
+    // The batch has been committed, let's see its status:
+    let time_just_before_getting_status = Utc::now();
+
+    let status = tracker
+        .get_batch_status(batch_id.clone())
+        .expect("successfully fetched batch status from server...")
+        .expect("...and it's not none");
+
+    // Just to make a meaningfull assertion about the BatchStatus's 'created_at' field:
+    assert!(status.created_at > time_just_before_batch_init);
+    assert!(status.created_at < time_just_before_getting_status);
+    assert_eq!(status.bid, batch_id);
+    assert_eq!(status.description, Some("Image resizing workload".into()));
+    assert_eq!(status.total, 3); // three jobs registered
+    assert_eq!(status.pending, 3); // and none executed just yet
+    assert_eq!(status.failed, 0);
+    // Docs do not mention it, but the golang client does:
+    // https://github.com/contribsys/faktory/blob/main/client/batch.go#L17-L19
+    assert_eq!(status.success_state, ""); // we did not even provide the 'success' callback
+    assert_eq!(status.complete_state, ""); // the 'complete' callback is pending
+
+    // consume and execute job 1 ...
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated"])
+        .unwrap();
+    assert!(had_one);
+
+    // ... and try consuming from the "callback" queue:
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated__CALLBACKs"])
+        .unwrap();
+    assert!(!had_one); // nothing in there
+
+    // let's ask the Faktory server about the batch status after
+    // we have consumed one job from this batch:
+    let status = tracker
+        .get_batch_status(batch_id.clone())
+        .expect("successfully fetched batch status from server...")
+        .expect("...and it's not none");
+
+    // this is because we have just consumed and executed 1 of 3 jobs:
+    assert_eq!(status.total, 3);
+    assert_eq!(status.pending, 2);
+    assert_eq!(status.failed, 0);
+
+    // now, consume and execute job 2
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated"])
+        .unwrap();
+    assert!(had_one);
+
+    // ... and the "callback" queue again:
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated__CALLBACKs"])
+        .unwrap();
+    assert!(!had_one); // not just yet ...
+
+    // let's check batch status once again:
+    let status = tracker
+        .get_batch_status(batch_id.clone())
+        .expect("successfully fetched batch status from server...")
+        .expect("...and it's not none");
+
+    // this is because we have just consumed and executed 2 of 3 jobs:
+    assert_eq!(status.total, 3);
+    assert_eq!(status.pending, 1);
+    assert_eq!(status.failed, 0);
+
+    // finally, consume and execute job 3 - the last one from the batch
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated"])
+        .unwrap();
+    assert!(had_one);
+
+    // let's check batch status to see what happens after
+    // all the jobs from the batch have been executed:
+    let status = tracker
+        .get_batch_status(batch_id.clone())
+        .expect("successfully fetched batch status from server...")
+        .expect("...and it's not none");
+
+    // this is because we have just consumed and executed 2 of 3 jobs:
+    assert_eq!(status.total, 3);
+    assert_eq!(status.pending, 0);
+    assert_eq!(status.failed, 0);
+    assert_eq!(status.complete_state, "1"); // callback has been enqueued!!
+
+    // let's now consume from the "callback" queue:
+    let had_one = consumer
+        .run_one(0, &["test_batch_of_jobs_can_be_initiated__CALLBACKs"])
+        .unwrap();
+    assert!(had_one); // we successfully consumed the callback
+
+    // let's check batch status one last time:
+    let status = tracker
+        .get_batch_status(batch_id.clone())
+        .expect("successfully fetched batch status from server...")
+        .expect("...and it's not none");
+
+    // this is because we have just consumed and executed 2 of 3 jobs:
+    assert_eq!(status.complete_state, "2"); // means calledback successfully executed
+}
+
+#[test]
+fn test_batches_can_be_nested() {
+    skip_if_not_enterprise!();
+    let url = learn_faktory_url();
+
+    // Set up 'producer', 'consumer', and 'tracker':
+    let mut producer = Producer::connect(Some(&url)).unwrap();
+    let mut consumer = ConsumerBuilder::default();
+    consumer.register("jobtype", move |_job| -> io::Result<_> { Ok(()) });
+    let mut _consumer = consumer.connect(Some(&url)).unwrap();
+    let mut tracker =
+        Tracker::connect(Some(&url)).expect("job progress tracker created successfully");
+
+    // Prepare some jobs:
+    let parent_job1 = Job::builder("jobtype")
+        .queue("test_batches_can_be_nested")
+        .build();
+    let child_job_1 = Job::builder("jobtype")
+        .queue("test_batches_can_be_nested")
+        .build();
+    let child_job_2 = Job::builder("jobtype")
+        .queue("test_batches_can_be_nested")
+        .build();
+    let grand_child_job_1 = Job::builder("jobtype")
+        .queue("test_batches_can_be_nested")
+        .build();
+
+    // Sccording to Faktory docs:
+    // "The callback for a parent batch will not enqueue until the callback for the child batch has finished."
+    // See: https://github.com/contribsys/faktory/wiki/Ent-Batches#guarantees
+    let parent_cb_job = Job::builder("clean_up")
+        .queue("test_batches_can_be_nested__CALLBACKs")
+        .build();
+    let child_cb_job = Job::builder("clean_up")
+        .queue("test_batches_can_be_nested__CALLBACKs")
+        .build();
+    let grandchild_cb_job = Job::builder("clean_up")
+        .queue("test_batches_can_be_nested__CALLBACKs")
+        .build();
+
+    // batches start
+    let parent_batch =
+        Batch::builder("Parent batch".to_string()).with_success_callback(parent_cb_job);
+    let mut parent_batch = producer.start_batch(parent_batch).unwrap();
+    let parent_batch_id = parent_batch.id().to_owned();
+    parent_batch.add(parent_job1).unwrap();
+
+    let child_batch = Batch::builder("Child batch".to_string()).with_success_callback(child_cb_job);
+    let mut child_batch = parent_batch.start_batch(child_batch).unwrap();
+    let child_batch_id = child_batch.id().to_owned();
+    child_batch.add(child_job_1).unwrap();
+    child_batch.add(child_job_2).unwrap();
+
+    let grandchild_batch =
+        Batch::builder("Grandchild batch".to_string()).with_success_callback(grandchild_cb_job);
+    let mut grandchild_batch = child_batch.start_batch(grandchild_batch).unwrap();
+    let grandchild_batch_id = grandchild_batch.id().to_owned();
+    grandchild_batch.add(grand_child_job_1).unwrap();
+
+    grandchild_batch.commit().unwrap();
+    child_batch.commit().unwrap();
+    parent_batch.commit().unwrap();
+    // batches finish
+
+    let parent_status = tracker
+        .get_batch_status(parent_batch_id.clone())
+        .unwrap()
+        .unwrap();
+    assert_eq!(parent_status.description, Some("Parent batch".to_string()));
+    assert_eq!(parent_status.total, 1);
+    assert_eq!(parent_status.parent_bid, None);
+
+    let child_status = tracker
+        .get_batch_status(child_batch_id.clone())
+        .unwrap()
+        .unwrap();
+    assert_eq!(child_status.description, Some("Child batch".to_string()));
+    assert_eq!(child_status.total, 2);
+    assert_eq!(child_status.parent_bid, Some(parent_batch_id));
+
+    let grandchild_status = tracker
+        .get_batch_status(grandchild_batch_id)
+        .unwrap()
+        .unwrap();
+    assert_eq!(
+        grandchild_status.description,
+        Some("Grandchild batch".to_string())
+    );
+    assert_eq!(grandchild_status.total, 1);
+    assert_eq!(grandchild_status.parent_bid, Some(child_batch_id));
+}
